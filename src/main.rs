43905c4e2826e0d76af4659a3de90516e822587b
Dynamic_Pigeon <3040313336@qq.com>
2025-11-08 16:56:42 +0800

    试一下自动发版

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@ use crate::bing::process;
 mod bing;
 
 fn main() {
-    log4rs::init_file("log4rs.yaml", Default::default()).unwrap();
+    let _ = log4rs::init_file("log4rs.yaml", Default::default());
     process("config.json").unwrap();
 }
