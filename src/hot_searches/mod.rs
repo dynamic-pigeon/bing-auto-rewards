b147fae5126b9817f4c89c68eb145f2590d90626
Dynamic_Pigeon <3040313336@qq.com>
2025-11-10 17:37:29 +0800

    fix

--- src/hot_searches/mod.rs
+++ src/hot_searches/mod.rs
@@ -1,19 +1,21 @@
-use std::sync::{Arc, LazyLock, RwLock};
+use std::sync::{LazyLock, RwLock};
 
 use linkme::distributed_slice;
-use rand::seq::SliceRandom;
+use rand::seq::IndexedRandom;
 
 mod common;
 
-static HOT_WORDS: LazyLock<RwLock<Arc<Vec<String>>>> =
-    LazyLock::new(|| RwLock::new(Arc::new(Vec::new())));
+static HOT_WORDS: LazyLock<RwLock<Vec<String>>> = LazyLock::new(|| RwLock::new(Vec::new()));
 
 #[distributed_slice]
 pub static HOT_WORDS_PROVIDERS: [fn() -> anyhow::Result<Vec<String>>];
 
 pub(crate) fn get_hot_words(count: usize) -> Vec<String> {
     let hot_words = HOT_WORDS.read().unwrap();
-    hot_words.iter().take(count).cloned().collect()
+    hot_words
+        .choose_multiple(&mut rand::rng(), count)
+        .cloned()
+        .collect()
 }
 
 pub(crate) fn fetch_hot_words() -> anyhow::Result<()> {
@@ -26,9 +28,8 @@ pub(crate) fn fetch_hot_words() -> anyhow::Result<()> {
     }
     all_hot_words.sort();
     all_hot_words.dedup();
-    all_hot_words.shuffle(&mut rand::rng());
     let mut hot_words_lock = HOT_WORDS.write().unwrap();
-    *hot_words_lock = Arc::new(all_hot_words);
+    *hot_words_lock = all_hot_words;
     Ok(())
 }
 
